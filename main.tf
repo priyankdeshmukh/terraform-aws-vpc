f30e2a427b31c461e49875216cc9f2867cae5e78
priyankdeshmukh <priyankdeshmukh300@gmail.com>
2022-08-09 12:04:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,32 @@ output "subnet_id" {
   value = aws_subnet.test_subnet.id
 }
 
+
+resource "aws_internet_gateway" "test_gateway" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "practice"
+  }
+}
+
+
+resource "aws_route_table" "test_route_table" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test_gateway.id
+  }
+
+
+  tags = {
+    Name = "practice"
+  }
+}
+
+
+resource "aws_main_route_table_association" "test_route_table_asso" {
+  vpc_id         = aws_vpc.test_vpc.id
+  route_table_id = aws_route_table.test_route_table.id
+}
